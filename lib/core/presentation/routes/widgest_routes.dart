99b4c0bb3a31a6b4d051f4421ebbc9504fef6149
zxb <zhangzhxb520@gmail.com>
2020-04-14 12:55:58 +0800

    添加Image示例

--- lib/core/presentation/routes/widgest_routes.dart
+++ lib/core/presentation/routes/widgest_routes.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_ui_challenges/core/data/models/menu.dart';
 import 'package:flutter_ui_challenges/src/widgets/assets/icon_example.dart';
+import 'package:flutter_ui_challenges/src/widgets/assets/image_example.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 final List<dynamic> pages = [
   MenuItem(title: "资源、图标、图片", icon: FontAwesomeIcons.image, items: [
     SubMenuItem("Icon", IconExample(), path: IconExample.path),
+    SubMenuItem("Image", ImageExample(), path: ImageExample.path),
   ]),
 ];
 
