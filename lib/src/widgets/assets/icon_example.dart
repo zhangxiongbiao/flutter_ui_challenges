ddc08840cd861f974de3717b30649b47124878f2
zxb <zhangzhxb520@gmail.com>
2020-04-14 12:55:50 +0800

    icon示例添加appbar标题栏

--- lib/src/widgets/assets/icon_example.dart
+++ lib/src/widgets/assets/icon_example.dart
@@ -7,13 +7,18 @@ class IconExample extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Icon(
-          Icons.image,
-          size: 64.0,
-          color: Theme.of(context).primaryColor,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Icon'),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Icon(
+            Icons.image,
+            size: 64.0,
+            color: Theme.of(context).primaryColor,
+          ),
         ),
       ),
     );
